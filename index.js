05699c5fafd25410207346330fe79600c13462d1
shishir99111 <shishir99111@gmail.com>
2018-01-11 02:27:31 +0530

    complete wirth nodejs server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const http = require('http').Server(app);
+const PORT = process.env.PORT || 5570;
+const path = require('path');
+const { abi, provider, contractAddress } = require('./env');
+
+app.use(express.static('./public'));
+
+app.get(['/*'], (req, res) => {
+  res.render(__dirname + '/index.ejs', { abi, provider, contractAddress });
+});
+
+http.listen(PORT, () => {
+  console.log('listening on PORT ' + PORT);
+});
